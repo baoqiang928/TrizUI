eaa5e7bee876fae007d1f7cb650dc837452e3d13
baoqiang <baoqiang928@126.com>
2017-09-28 17:53:41 +0800

    1

---
+++ TrizUI/TrizUI/Test/Controllers/App.js
@@ -0,0 +1,21 @@
+﻿var myApp = angular.module("myApp", ["ui.router"]);
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/PageTab");
+    $stateProvider
+        .state("PageTab", {
+           url: "/PageTab",
+           templateUrl: "/Test/PageTab.html"
+       })
+       .state("PageTab.Page1", {
+           url:"/Page1",
+           templateUrl: "/Test/Page1.html"
+       })
+       .state("PageTab.Page2", {
+           url:"/Page2",
+           templateUrl: "/Test/Page2.html"
+       })
+       .state("PageTab.Page3", {
+           url:"/Page3",
+           templateUrl: "/Test/Page3.html"
+       });
+});
---
+++ TrizUI/TrizUI/pages/Controllers/App.js
@@ -0,0 +1,21 @@
+﻿var myApp = angular.module("myApp", ["ui.router"]);
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/PageTab");
+    $stateProvider
+        .state("PageTab", {
+           url: "/PageTab",
+           templateUrl: "/pages/PageTab.html"
+       })
+       .state("PageTab.Page1", {
+           url:"/Page1",
+           templateUrl: "/pages/Page1.html"
+       })
+       .state("PageTab.Page2", {
+           url:"/Page2",
+           templateUrl: "/pages/Page2.html"
+       })
+       .state("PageTab.Page3", {
+           url:"/Page3",
+           templateUrl: "/pages/Page3.html"
+       });
+});
