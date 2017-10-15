3f93ecfd2a5631b81bb1b05fd3af8cd476e07bb5
baoqiang <baoqiang928@126.com>
2017-10-15 16:35:51 +0800

    1

---
+++ TrizUI/TrizUI/Talent/Personnel/Controllers/App.js
@@ -0,0 +1,80 @@
+﻿var myApp = angular.module("myApp", ["ui.router"]);
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/Profile");
+    $stateProvider
+        .state("Profile", {
+            url: "/Profile",
+            templateUrl: "/Talent/Personnel/Profile/ProfileList.html",
+            controller: 'table-Controller'
+        })
+       .state("JobRequest", {
+           url: "/JobRequest",
+           templateUrl: "/Talent/Personnel/JobRequest/JobRequestList.html"
+       })
+       .state("TechRequest", {
+           url: "/TechRequest",
+           templateUrl: "/Talent/Personnel/TechRequest/TechRequestList.html"
+       })
+        .state("Favorite", {
+            url: "/Favorite",
+            templateUrl: "/Talent/Personnel/Favorite/FavoriteList.html"
+        })
+       .state("QuestionAnalyseAdd", {
+           url: "/AnalyseAdd",
+           templateUrl: "/pages/QuestionAnalyse/QuestionAnalyseOperate.html"
+       })
+       .state("FunctionAnalyseAdd", {
+           url: "/FunctionAnalyseAdd",
+           templateUrl: "/pages/FunctionAnalyse/FunctionAnalyseOperate.html",
+           controller: 'tree-Controller'
+       })
+       .state("StandardSolutionList", {
+           url: "/StandardSolutionList",
+           templateUrl: "/pages/StandardSolution/StandardSolutionList.html"
+       })
+       .state("CauseEffectAdd", {
+           url: "/CauseEffectAdd",
+           templateUrl: "/pages/CauseEffect/CauseEffectOperate.html"
+       })
+       .state("PhysicalConflictList", {
+           url: "/PhysicalConflictList",
+           templateUrl: "/pages/Conflict/PhysicalConflictList.html"
+       })
+       .state("TechConflictList", {
+           url: "/TechConflictList",
+           templateUrl: "/pages/Conflict/TechConflictList.html"
+       })
+       .state("TechConflictResolveList", {
+           url: "/TechConflictResolveList",
+           templateUrl: "/pages/Conflict/TechConflictResolveList.html",
+           controller: 'tree-Controller'
+       })
+       .state("PhysicalConflictResolveList", {
+           url: "/PhysicalConflictResolveList",
+           templateUrl: "/pages/Conflict/PhysicalConflictResolveList.html",
+           controller: 'tree-Controller'
+       })
+       .state("EvolveList", {
+           url: "/EvolveList",
+           templateUrl: "/pages/Evolve/EvolveList.html",
+           controller: 'tree-Controller'
+       })
+       .state("EvolveOperate", {
+           url: "/EvolveOperate",
+           templateUrl: "/pages/Evolve/EvolveOperate.html",
+           controller: 'tree-Controller'
+       })
+       .state("PageTab.Page1", {
+           url: "/Page1",
+           templateUrl: "/pages/Page1.html"
+       })
+       .state("PageTab.Page2", {
+           url: "/Page2",
+           templateUrl: "/pages/Page2.html"
+       })
+       .state("Page3", {
+           url: "/Page3",
+           templateUrl: "/pages/Page3.html"
+       });
+});
+
---
+++ TrizUI/TrizUI/Talent/Personnel/Controllers/tb.js
@@ -0,0 +1,39 @@
+﻿angular.module("myApp")
+    .controller('table-Controller', function ($scope) {
+        var oTable1 = $('#sample-table-2').dataTable({
+            "aoColumns": [
+              { "bSortable": false },
+              { "bSortable": false }, { "bSortable": false }, { "bSortable": false }, { "bSortable": false }, { "bSortable": false },
+              { "bSortable": false }
+            ]
+        });
+
+
+        $('table th input:checkbox').on('click', function () {
+            var that = this;
+            $(this).closest('table').find('tr > td:first-child input:checkbox')
+            .each(function () {
+                this.checked = that.checked;
+                $(this).closest('tr').toggleClass('selected');
+            });
+
+        });
+
+
+        $('[data-rel="tooltip"]').tooltip({ placement: tooltip_placement });
+        function tooltip_placement(context, source) {
+            var $source = $(source);
+            var $parent = $source.closest('table')
+            var off1 = $parent.offset();
+            var w1 = $parent.width();
+
+            var off2 = $source.offset();
+            var w2 = $source.width();
+
+            if (parseint(off2.left) < parseint(off1.left) + parseint(w1 / 2)) return 'right';
+            return 'left';
+        }
+
+
+
+    });
