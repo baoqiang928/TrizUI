9f0f32b79ec06d0629895ac1b2d085599218c0d3
baoqiang <baoqiang928@126.com>
2017-11-09 10:37:39 +0800

    3

--- TrizUI/Triz.DAL/ProjectDAL.cs
+++ TrizUI/Triz.DAL/ProjectDAL.cs
@@ -5,23 +5,117 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Triz.Model;
+using System.Transactions;
 
 namespace Triz.DAL
 {
     public class ProjectDAL
     {
-        public bool AddEntity(ProjectInfo ProjectInfo)//添加实体  
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="ProjectInfo"></param>
+        /// <returns></returns>
+        public bool AddEntity(ProjectInfo ProjectInfo)
         {
-            TrizDBEntities entity = new TrizDBEntities();//定义上下文实体  
-            tbl_ProjectInfo projectdata = new tbl_ProjectInfo();
-            projectdata.Name = ProjectInfo.Name;
-            entity.tbl_ProjectInfo.Add(projectdata);
-            int count = entity.SaveChanges();
-            if (count > 0)
+            using (TrizDBEntities TrizDB = new TrizDBEntities())
             {
-                return true;
+                try
+                {
+                    TrizDB.tbl_ProjectInfo.Add(GetDataEntity(ProjectInfo));
+                    if (TrizDB.SaveChanges() > 0)
+                        return true;
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+                {
+                    Exception raise = dbEx;
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            string message = string.Format("{0},{1}",
+                                validationErrors.Entry.Entity.ToString(),
+                                validationError.ErrorMessage);
+                            raise = new InvalidOperationException(message, raise);
+                            throw raise;
+                        }
+                    }
+                }
             }
             return false;
+        }
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="ProjectInfo"></param>
+        /// <returns></returns>
+        public bool UpdateEntity(ProjectInfo ProjectInfo)
+        {
+            bool result = false;
+            using (TrizDBEntities TrizDB = new TrizDBEntities())
+            {
+                try
+                {
+                    tbl_ProjectInfo ProjectInfoEntity = new tbl_ProjectInfo();
+                    ProjectInfoEntity.Name = ProjectInfo.Name;
+                    TrizDB.SaveChanges();
+                    if (TrizDB.SaveChanges() > 0)
+                        result = true;
+                    return result;
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+                {
+                    Exception raise = dbEx;
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            string message = string.Format("{0},{1}",
+                                validationErrors.Entry.Entity.ToString(),
+                                validationError.ErrorMessage);
+                            raise = new InvalidOperationException(message, raise);
+                            throw raise;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 删除记录
+        /// </summary>
+        /// <param name="ProjectInfo"></param>
+        /// <returns></returns>
+        public int Delete(ProjectInfo ProjectInfo)
+        {
+            using (TrizDBEntities TrizDB = new TrizDBEntities())
+            {
+                try
+                {
+                    var Query = TrizDB.tbl_ProjectInfo.Where(o => o.ID == ProjectInfo.ID).First();
+                    if (Query == null) return 0;
+                    TrizDB.tbl_ProjectInfo.Remove(Query);
+                    return TrizDB.SaveChanges();
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+                {
+                    Exception raise = dbEx;
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            string message = string.Format("{0},{1}",
+                                validationErrors.Entry.Entity.ToString(),
+                                validationError.ErrorMessage);
+                            raise = new InvalidOperationException(message, raise);
+                            throw raise;
+                        }
+                    }
+                }
+            }
+            return 0;
         }
 
         public List<ProjectInfo> Query(string name, int pageIndex, int pageSize, ref int totalItems, ref int PagesLength)
@@ -30,25 +124,42 @@ namespace Triz.DAL
             Expression<Func<tbl_ProjectInfo, bool>> where = PredicateExtensionses.True<tbl_ProjectInfo>();
             if (!string.IsNullOrWhiteSpace(name))
                 where = where.And(a => a.Name == name);
-            using (TrizDBEntities entity = new TrizDBEntities())
+            using (TrizDBEntities TrizDB = new TrizDBEntities())
             {
-
-                var query = entity.tbl_ProjectInfo.Where(where.Compile());
+                var query = TrizDB.tbl_ProjectInfo.Where(where.Compile());
                 totalItems = query.Count();
-                PagesLength = (int)Math.Ceiling((double)totalItems/pageSize);
+                PagesLength = (int)Math.Ceiling((double)totalItems / pageSize);
                 query = query.OrderBy(p => p.ID).Skip(startRow).Take(pageSize);
-                List<tbl_ProjectInfo> ProjectDataEntityList = query.ToList();
-                List<ProjectInfo> ProjectList = new List<ProjectInfo>();
-                //Mapper.CreateMap<ProjectDataEntityList, ProjectList>(); 
-                foreach (tbl_ProjectInfo tbl_ProjectInfo in ProjectDataEntityList)
-                {
-                    ProjectInfo ProjectInfo = new ProjectInfo();
-                    ProjectInfo.ID = tbl_ProjectInfo.ID;
-                    ProjectInfo.Name = tbl_ProjectInfo.Name;
-                    ProjectList.Add(ProjectInfo);
-                }
-                return ProjectList;
+                return GetGetBusinessObjectList(query.ToList());
             }
+        }
+
+        public ProjectInfo GetBusinessObject(tbl_ProjectInfo ProjectInfoEntity)
+        {
+            ProjectInfo ProjectInfo = new ProjectInfo();
+            ProjectInfo.ID = ProjectInfoEntity.ID;
+            ProjectInfo.Name = ProjectInfoEntity.Name;
+
+            return ProjectInfo;
+        }
+
+        public tbl_ProjectInfo GetDataEntity(ProjectInfo ProjectInfo)
+        {
+            tbl_ProjectInfo ProjectInfoEntity = new tbl_ProjectInfo();
+            ProjectInfoEntity.ID = ProjectInfo.ID ?? 0;
+            ProjectInfoEntity.Name = ProjectInfo.Name;
+
+            return ProjectInfoEntity;
+        }
+
+        public List<ProjectInfo> GetGetBusinessObjectList(List<tbl_ProjectInfo> ProjectInfoEntityList)
+        {
+            List<ProjectInfo> ProjectInfoList = new List<ProjectInfo>();
+            foreach (tbl_ProjectInfo tbl_ProjectInfo in ProjectInfoEntityList)
+            {
+                ProjectInfoList.Add(GetBusinessObject(tbl_ProjectInfo));
+            }
+            return ProjectInfoList;
         }
     }
 }
