2c801420f63b2266e0aec56686413895927f7d18
baoqiang <baoqiang928@126.com>
2017-11-10 14:39:07 +0800

    model1

--- TrizUI/TrizUI/pages/Controllers/Project.js
+++ TrizUI/TrizUI/pages/Controllers/Project.js
@@ -1,8 +1,6 @@
 ﻿angular.module("myApp")
     .controller('ProjectCtrl', function ($scope, $location, requestService, $state) {
-
         var Sources = "Projects";
-        $scope.abcd = "1234";
         $scope.paginationConf = {
             currentPage: 1,
             totalItems: 8000,
@@ -49,15 +47,24 @@
         $scope.BatchDelete = function () {
             var ids = "";
             $('input[name="ck"]:checked').each(function () {
-                //alert(1);
-                //var sfruit=$(this).val();  
-                //alert(sfruit);
-                //var orders=sfruit.split(",");
+                ids = $(this).val() + "^" + ids;
             });
 
-            //requestService.batchdelete(Sources, ids).then(function (data) {
-            //    GetProjects();
-            //});
+            if (ids == "") {
+                Alert("请选择记录。");
+                return;
+            }
+            bootbox.confirm("要删除选中的所有记录？", function (result) {
+                if (result) {
+                    requestService.batchdelete(Sources, ids).then(function (data) {
+                        GetProjects();
+                        $scope.one = false;
+                        $scope.all = false;
+                    });
+                }
+            });
+
+
         }
 
 
@@ -65,55 +72,7 @@
             $state.go("ProjectAdd", { ID: ID });
         }
 
-        $scope.selected = [];
-        var updateSelected = function (action, id, name) {
-            if (action == 'add' && $scope.selected.indexOf(id) == -1) {
-                $scope.selected.push(id);
-            }
-            if (action == 'remove' && $scope.selected.indexOf(id) != -1) {
-                var idx = $scope.selected.indexOf(id);
-                $scope.selected.splice(idx, 1);
-            }
-        }
-
-        $scope.updateSelection = function ($event, id) {
-            var checkbox = $event.target;
-            var action = (checkbox.checked ? 'add' : 'remove');
-            updateSelected(action, id, checkbox.name);
-            alert($scope.selected);
-        }
-
-        $scope.isSelected = function (id) {
-            return $scope.selected.indexOf(id) >= 0;
-        }
-
-        //$("#checkall").click(
-        //  function () {
-        //      if (this.checked) {
-        //          $("input[name='ck']").attr('checked', true);
-        //      } else {
-        //          $("input[name='ck']").attr('checked', false);
-        //      }
-        //  });
-
-        //$(function(){  
-        //    $("#checkall").click(function () {
-        //        //第一种方法 全选全不选  
-        //        if(this.checked){   
-        //            $("input[name='ck']:checkbox").attr('checked',true);   
-        //        }else{   
-        //            $("input[name='ck']:checkbox").attr('checked', false);
-        //        }  
-        //        //第二种方法 全选全不选   
-        //        $('[name=ck]:checkbox').attr('checked', this.checked);//checked为true时为默认显示的状态   
-        //    });  
-        //    $("#checkrev").click(function(){  
-        //        //实现反选功能  
-        //        $('[name=ck]:checkbox').each(function () {
-        //            this.checked=!this.checked;  
-        //        });  
-        //    });   
-        //});
 
 
     });
+
