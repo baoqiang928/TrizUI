f16ba3f79432745348b084befd6a795d15dd6c39
baoqiang <baoqiang928@126.com>
2017-11-10 17:13:33 +0800

    validate

--- TrizUI/TrizUI/pages/Controllers/ProjectOpeCtrl.js
+++ TrizUI/TrizUI/pages/Controllers/ProjectOpeCtrl.js
@@ -9,33 +9,126 @@
         };
 
         requestService.getobj(Sources, $stateParams.ID).then(function (data) {
-            //alert(data.Name);
-            //if (data._code === 200) {
             $scope.data = data;
-            //alert($scope.projects.length);
-            //alert($scope.projects[0].Id);
-            //};
         });
 
         $scope.Save = function () {
 
+            //alert($('#validation-form').valid());
+
+            if (!$('#validation-form').valid()) {
+                return false;
+            }
+
+
+
             if ($stateParams.ID == "") {
                 requestService.add(Sources, $scope.data).then(function (data) {
-                    //$state.go("ProjectList", {}, { reload: true, cache: false });
                     $state.go("ProjectList");
                 });
                 return;
             }
 
             requestService.update(Sources, $scope.data).then(function (data) {
-                //$state.go("ProjectList", {}, { reload: true, cache: false });
                 $state.go("ProjectList");
             });
-            //$state.go("ProjectList");
-            //$scope.$apply();
         };
 
 
+        $('#validation-form').validate({
+            errorElement: 'div',
+            errorClass: 'help-block',
+            focusInvalid: false,
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                },
+                password2: {
+                    required: true,
+                    minlength: 5,
+                    equalTo: "#password"
+                },
+                name: {
+                    required: true
+                },
+                phone: {
+                    required: true,
+                    phone: 'required'
+                },
+                url: {
+                    required: true,
+                    url: true
+                },
+                comment: {
+                    required: true
+                },
+                state: {
+                    required: true
+                },
+                platform: {
+                    required: true
+                },
+                subscription: {
+                    required: true
+                },
+                gender: 'required',
+                agree: 'required'
+            },
+
+            messages: {
+                email: {
+                    required: "Please provide a valid email.",
+                    email: "Please provide a valid email."
+                },
+                password: {
+                    required: "Please specify a password.",
+                    minlength: "Please specify a secure password."
+                },
+                subscription: "Please choose at least one option",
+                gender: "Please choose gender",
+                agree: "Please accept our policy"
+            },
+
+            invalidHandler: function (event, validator) { //display error alert on form submit   
+                $('.alert-danger', $('.login-form')).show();
+            },
+
+            highlight: function (e) {
+                $(e).closest('.form-group').removeClass('has-info').addClass('has-error');
+            },
+
+            success: function (e) {
+                $(e).closest('.form-group').removeClass('has-error').addClass('has-info');
+                $(e).remove();
+            },
+
+            errorPlacement: function (error, element) {
+                if (element.is(':checkbox') || element.is(':radio')) {
+                    var controls = element.closest('div[class*="col-"]');
+                    if (controls.find(':checkbox,:radio').length > 1) controls.append(error);
+                    else error.insertAfter(element.nextAll('.lbl:eq(0)').eq(0));
+                }
+                else if (element.is('.select2')) {
+                    error.insertAfter(element.siblings('[class*="select2-container"]:eq(0)'));
+                }
+                else if (element.is('.chosen-select')) {
+                    error.insertAfter(element.siblings('[class*="chosen-container"]:eq(0)'));
+                }
+                else error.insertAfter(element.parent());
+            },
+
+            submitHandler: function (form) {
+            },
+            invalidHandler: function (form) {
+            }
+        });
 
 
-    });
+
+
+    });//end
