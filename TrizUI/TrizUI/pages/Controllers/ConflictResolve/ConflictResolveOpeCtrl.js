5fd07d941e21eb29215611c991d31c631143e975
baoqiang <baoqiang928@126.com>
2018-04-06 22:34:40 -0600

    解决思路

--- TrizUI/TrizUI/pages/Controllers/ConflictResolve/ConflictResolveOpeCtrl.js
+++ TrizUI/TrizUI/pages/Controllers/ConflictResolve/ConflictResolveOpeCtrl.js
@@ -1,6 +1,33 @@
 ﻿angular.module('myApp')
     .controller('ConflictResolveOpeCtrl', function ($scope, $location, requestService, $state, locals, $stateParams) {
-
+        $scope._simpleConfig = {
+            //初始化编辑器内容  
+            content: '<p>test1</p>',
+            //是否聚焦 focus默认为false  
+            focus: true,
+            //首行缩进距离,默认是2em  
+            indentValue: '2em',
+            //初始化编辑器宽度,默认1000  
+            initialFrameWidth: '100%',
+            //初始化编辑器高度,默认320  
+            initialFrameHeight: 520,
+            //编辑器初始化结束后,编辑区域是否是只读的，默认是false  
+            readonly: false,
+            //启用自动保存  
+            enableAutoSave: false,
+            //自动保存间隔时间， 单位ms  
+            saveInterval: 500,
+            //是否开启初始化时即全屏，默认关闭  
+            fullscreen: false,
+            //图片操作的浮层开关，默认打开  
+            imagePopup: true,
+            //提交到后台的数据是否包含整个html字符串  
+            allHtmlEnabled: false,
+            //额外功能添加                 
+            functions: ['map', 'insertimage', 'insertvideo', 'attachment',
+            , 'insertcode', 'webapp', 'template',
+            'background', 'wordimage']
+        };
         $scope.ImproveCharacter = $stateParams.ImproveCharacter;
         $scope.DeteriorateCharacter = $stateParams.DeteriorateCharacter;
         console.log("$stateParams", $stateParams);
@@ -48,7 +75,15 @@
             newobj.CaseName = $scope.nodeData.Name;
             $scope.ConflictResolveInfoList.push(newobj);
         }
+        $scope.Solution = "";
+        $scope.CurrentIndex = "";
 
+        $scope.EditConflictResolveInfo = function (Solution, index) {
+            $scope.Solution = Solution;
+            $scope.CurrentIndex = index;
+            $('#modal-table').modal('show');
+            return;
+        };
         $scope.DeleteConflictResolveInfo = function (index) {
             bootbox.confirm("要删除当前的记录？", function (result) {
                 if (result) {
@@ -78,4 +113,11 @@
         $scope.$on("nodeData", function (event, msg) {
             $scope.nodeData = msg;
         });
+
+        $scope.SaveToList = function () {
+            $scope.ConflictResolveInfoList[$scope.CurrentIndex].Remark = $scope.Solution;
+            console.log("$scope.ConflictResolveInfoList", $scope.ConflictResolveInfoList);
+            $scope.Solution = "";
+            $('#modal-table').modal('hide');
+        };
     });
